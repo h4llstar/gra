40d269ed118cf99210dd814b91eb3e77aa239c01
miacheats <coolgamingforme@gmail.com>
2025-02-09 22:19:22 -0800

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -5748,11 +5748,6 @@ mainapi:CreateCategory({
 	Icon = getcustomasset('newvape/assets/new/miniicon.png'),
 	Size = UDim2.fromOffset(19, 12)
 })
-mainapi:CreateCategory({
-	Name = 'Modules',
-	Icon = getcustomasset('newvape/assets/new/module.png'),
-	Size = UDim2.fromOffset(14, 14)
-})
 mainapi.Categories.Main:CreateDivider('misc')
 
 --[[

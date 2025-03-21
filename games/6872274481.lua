23b9d103d8ef168c20b7136a2f2d4a110ee7c5c6
h4llstar <jadenblackerson@gmail.com>
2025-03-21 23:21:20 +0000

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -1376,29 +1376,30 @@ run(function()
 end)
 	
 run(function()
-	local Value
-	
-	Reach = vape.Categories.Combat:CreateModule({
-		Name = 'Reach',
-		Function = function(callback)
-			bedwars.CombatConstant.RAYCAST_SWORD_CHARACTER_DISTANCE = callback and Value.Value + 2 or 14.4
-		end,
-		Tooltip = 'Extends attack reach'
-	})
-	Value = Reach:CreateSlider({
-		Name = 'Range',
-		Min = 0,
-		Max = 18,
-		Default = 18,
-		Function = function(val)
-			if Reach.Enabled then
-				bedwars.CombatConstant.RAYCAST_SWORD_CHARACTER_DISTANCE = val + 2
-			end
-		end,
-		Suffix = function(val)
-			return val == 1 and 'stud' or 'studs'
-		end
-	})
+    local Value
+
+    Reach = vape.Categories.Combat:CreateModule({
+        Name = 'Reach',
+        Function = function(callback)
+            bedwars.CombatConstant.RAYCAST_SWORD_CHARACTER_DISTANCE = callback and Value.Value + 10 or 14.4
+        end,
+        Tooltip = 'Extends attack reach'
+    })
+
+    Value = Reach:CreateSlider({
+        Name = 'Range',
+        Min = 0,
+        Max = 30,
+        Default = 30,
+        Function = function(val)
+            if Reach.Enabled then
+                bedwars.CombatConstant.RAYCAST_SWORD_CHARACTER_DISTANCE = val + 10
+            end
+        end,
+        Suffix = function(val)
+            return val == 1 and 'stud' or 'studs'
+        end
+    })
 end)
 	
 run(function()
